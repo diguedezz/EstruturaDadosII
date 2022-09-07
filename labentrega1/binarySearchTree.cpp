8c7b13aaf2a3b012b1a117e298bfe00117021b14
Pedro-Moreno <60677791+Pedro-Moreno@users.noreply.github.com>
2022-09-06 21:11:33 -0300

    Projeto final

--- labentrega1/binarySearchTree.cpp
+++ labentrega1/binarySearchTree.cpp
@@ -1,11 +1,11 @@
 /*
-    Implementa��o de Classe baseando-se nos exemplos do livro "Introduction to algorithms" por Cormen.
-    Representa��o do tipo abstrato de dado: �rvore de busca bin�ria
+	Implementa��o de Classe baseando-se nos exemplos do livro "Introduction to algorithms" por Cormen.
+	Representa��o do tipo abstrato de dado: �rvore de busca bin�ria
 
-    C�digo escrito por Diego Guedes, aluno do 4� per�odo de Estrutura de Dados II, 
-    do curso Ci�ncia da Computa��o da Universidade Presbiteriana Mackenzie.
+	C�digo escrito por Diego Guedes, aluno do 4� per�odo de Estrutura de Dados II,
+	do curso Ci�ncia da Computa��o da Universidade Presbiteriana Mackenzie.
 
-    Ter�a-Feira, 16 de agosto de 2022, 03:55 da manh�. 
+	Ter�a-Feira, 16 de agosto de 2022, 03:55 da manh�.
 
 */
 
@@ -14,45 +14,54 @@
 
 using namespace std;
 
-class binarySearchTree(){
-    private:
-        Node* source;
+class binarySearchTree
+{
+private:
+	Node *source;
 
-    public:
-    	binarySearchTree(){
-    		source = nullptr;
-		}
-		
-//******************************************************************
-	void insert(int key){
-		if(source == nullptr) // verifica se a ?rvore est? vazia
+public:
+	binarySearchTree()
+	{
+		source = nullptr;
+	}
+
+	//******************************************************************
+	void insert(int key)
+	{
+		if (source == nullptr)		// verifica se a ?rvore est? vazia
 			source = new Node(key); // cria um novo n?
 		else
 			insertAux(source, key);
 	}
 
-
-	void insertAux(Node *node, int key){
+	void insertAux(Node *node, int key)
+	{
 		// se for menor, ent?o insere ? esquerda
-		if(key < node->getKey()){
+		if (key < node->getKey())
+		{
 			// verifica se a esquerda ? nulo
-			if(node->getLeft() == nullptr){
+			if (node->getLeft() == nullptr)
+			{
 				Node *newNode = new Node(key);
 				node->setLeft(newNode); // seta o novo_no ? esquerda
 			}
-			else{
+			else
+			{
 				// sen?o, continua percorrendo recursivamente
 				insertAux(node->getLeft(), key);
 			}
 		}
 		// se for maior, ent?o insere ? direita
-		else if(key > node->getKey()){
+		else if (key > node->getKey())
+		{
 			// verifica se a direita ? nulo
-			if(node->getRight() == nullptr){
+			if (node->getRight() == nullptr)
+			{
 				Node *newNode = new Node(key);
 				node->setRight(newNode); // seta o novo_no ? direita
 			}
-			else{
+			else
+			{
 				// sen?o, continua percorrendo recursivamente
 				insertAux(node->getRight(), key);
 			}
@@ -61,124 +70,176 @@ class binarySearchTree(){
 		// n?o pode existir 2 chaves iguais
 	}
 
+	Node *getSource() { return this->source; }
 
-	Node* getSource(){return this->source;}
-
-
-	//Inorder-Tree-Walk(x)
-	void inOrder(Node* node){
-		if(node != nullptr){
+	// Inorder-Tree-Walk(x)
+	void inOrder(Node *node)
+	{
+		if (node != nullptr)
+		{
 			inOrder(node->getLeft());
 			cout << node->getKey() << " ";
 			inOrder(node->getRight());
 		}
 	}
 
-	//preorder-Tree-Walk(x)
-	void preOrder(Node* node){
-		if(node != nullptr){
+	// preorder-Tree-Walk(x)
+	void preOrder(Node *node)
+	{
+		if (node != nullptr)
+		{
 			cout << node->getKey() << " ";
 			preOrder(node->getLeft());
 			preOrder(node->getRight());
 		}
 	}
-	
-	//posorder-Tree-Walk(x)
-	void posOrder(Node* node){
-		if(node != nullptr){
+
+	// posorder-Tree-Walk(x)
+	void posOrder(Node *node)
+	{
+		if (node != nullptr)
+		{
 			posOrder(node->getLeft());
 			posOrder(node->getRight());
 			cout << node->getKey() << " ";
 		}
 	}
-	
-//ATIVIDADES 1
-	
-    
-    //Tree-Search(x,k)
-    //pesquisarRec(int chave)
-	Node* recursiveSearch(Node* node, int key){
-		if(node == nullptr || key == node->getKey())
+
+	// ATIVIDADES 1
+
+	// Tree-Search(x,k)
+	// pesquisarRec(int chave)
+	Node *recursiveSearch(Node *node, int key)
+	{
+		if (node == nullptr)
+		{
+			cout << "\nNão foi achado!";
 			return node;
-		if(key < node->getKey)
-			return recursiveSearch(Node* node->getLeft(), key);
-		else return recursiveSearch(Node* node->getRight(), key);
+		}
+		if (key == node->getKey())
+		{
+			cout << "\nSucesso!";
+			return node;
+		}
+
+		if (key < node->getKey())
+			return recursiveSearch(node->getLeft(), key);
+		else
+			return recursiveSearch(node->getRight(), key);
 	}
 
-    //iterative-Tree-Search(x,k)
-    //pesquisarIter(int chave)
-    Node* iterativeSearch(Node* node, int key){
-        while (node != nullptr){
-            if (key < node->getKey())
-                node = node->getLeft();
-            else
-                node = node->getRight();
-        }
-        return node;
-    }
+	// iterative-Tree-Search(x,k)
+	// pesquisarIter(int chave)
+	Node *iterativeSearch(Node *node, int key)
+	{
+		while (node != nullptr)
+		{
+			if (node->getKey() == key)
+			{
+				cout << "\nAchado!";
+				return node;
+			}
 
-	void auxOrder(Node* node){
-		if(node != nullptr){
+			if (key < node->getKey())
+				node = node->getLeft();
+			else
+				node = node->getRight();
+		}
+
+		return node;
+	}
+
+	void auxOrder(Node *node)
+	{
+		if (node != nullptr)
+		{
 			auxOrder(node->getLeft());
 			auxOrder(node->getRight());
 		}
 	}
 
-	//qdeNos()
-	int nodeCounter(){
-
-        Node* node = this->source;
-		int counter;
-
-		while(node != nullptr){
-			counter++;
-			auxOrder(node->getLeft());
-			auxOrder(node->getRight());
+	// qdeNos()
+	int nodeCounter(Node *node)
+	{
+		if (node != nullptr)
+		{
+			return 1 + nodeCounter(node->getLeft()) + nodeCounter(node->getRight());
 		}
-		
-		return counter;
-	}
-	
-    //alturaBST()
-    int treeHeight(){
-		Node* node = this->source;
-    	int height;
-
-		if(node == nullptr)
-			return height;
-		else{
-			if
+		else
+		{
+			return 0;
 		}
 	}
 
-    //Tree-Minimum(x)
-    //min()
-    Node* minimumNode(){
-        Node* node = this->source;
-        while (node->getLeft() != nullptr)
-            node = node->getLeft();
-        return node;
-    }
-    
-    //Tree-Maximum(x)
-    //max()
-    Node* maximumNode(){
-        Node* node = this->source;
-        while (node->getRight() != nullptr)
-            node = node->getRight();
-        return node;
-    }
+	// alturaBST()
+	int treeHeight()
+	{
+		Node *node = this->source;
 
-    //folhas()
-    int keyLeafs(){
-		int leafs;
-		return leafs;
+		if (node == nullptr)
+			return -1;
+		else
+		{
+			int esq = treeHeight();
+			int dir = treeHeight();
+			if (esq > dir)
+				return esq + 1;
+			else
+				return dir + 1;
+		}
 	}
-    
-    //removerFolha(int chave)
-    void deleteLeaf(int key){
-		Node* node;
+
+	// Tree-Minimum(x)
+	// min()
+	Node *minimumNode()
+	{
+		Node *node = this->source;
+		while (node->getLeft() != nullptr)
+			node = node->getLeft();
+		return node;
+	}
+
+	// Tree-Maximum(x)
+	// max()
+	Node *maximumNode()
+	{
+		Node *node = this->source;
+		while (node->getRight() != nullptr)
+			node = node->getRight();
+		return node;
+	}
+
+	// folhas()
+	void keyLeafs(Node *node)
+	{
+		if (node != nullptr)
+		{
+			keyLeafs(node->getLeft());
+			keyLeafs(node->getRight());
+			if (node->getLeft() == nullptr && node->getRight() == nullptr)
+			{
+				cout << node->getKey() << " ";
+			}
+		}
+	}
+
+	// removerFolha(int chave)
+	Node *deleteLeaf(int key)
+	{
+		Node *node;
 		node = iterativeSearch(this->source, key);
-		node = nullptr;		
+
+		if (node->getLeft() == nullptr && node->getRight() == nullptr) // é uma folha
+		{
+			delete node;
+			node = nullptr;
+			cout << "\nDeletado!";
+			return node;
+		}
+		else
+		{
+			cout << "\nNão é uma folha";
+			return node;
+		}
 	}
-}
+};
--- labentrega1/main.cpp
+++ labentrega1/main.cpp
@@ -1,59 +1,130 @@
 #include <iostream>
 #include "binarySearchTree.cpp"
-#include "Node.h"
 
 using namespace std;
 
 int main(int argc, char *argv[])
 {
 	binarySearchTree tree;
-    int option, x;
-    cout << ("\nTestando o TAD BST (Elementos do tipo int)\n");
+	// tree.insert(10);
+	// tree.insert(20);
+	// tree.insert(5);
+	// tree.insert(2);
+	int option, x;
+	cout << ("\nTestando o TAD BST (Elementos do tipo int)\n");
 
-	do {
-    	cout << "\n\n";
-            <<  "\n***********************************"
-            <<  "\n\tMenu - Arvore BST\n"
-            <<  "\nEntre com a opcao:"
-		    <<  "\n ----1: Inserir"
-		    <<  "\n ----2: Excluir"
-		    <<  "\n ----3: Pesquisar"
-		    <<  "\n ----4: Imprimir em ordem"
-		    <<  "\n ----5: Sair do programa\n"
-		    <<  "\n***********************************"
-		    <<  "\n-> ";
+	do
+	{
+		cout << "\n\n"
+			 << "\n***********************************"
+			 << "\n\tMenu - Arvore BST\n"
+			 << "\nEntre com a opcao:"
+			 << "\n ----1: Inserir"
+			 << "\n ----2: Imprimir em ordem"
+			 << "\n ----3: Imprimir em Pré-ordem"
+			 << "\n ----4: Imprimir em Pós-ordem"
+			 << "\n ----5: Pesquisa recursiva"
+			 << "\n ----6: Pesquisa iterativa"
+			 << "\n ----7: Quantidade de nós"
+			 << "\n ----8: Altura da árvore"
+			 << "\n ----9: Nó mínimo"
+			 << "\n ----10: Nó máximo"
+			 << "\n ----11: Imprimir Folhas em ordem crescente"
+			 << "\n ----12: Deletar de Folhas"
+			 << "\n ----0: Sair do programa\n"
+			 << "\n***********************************"
+			 << "\n-> ";
 		cin >> option;
-        
-		switch(option) {
-	 		case 1: {
-		       cout << "\n Informe o valor (int) -> ";
-		       cin >> x;
-		       tree.insert(x);
-		       break;
-			}
-			case 2: {
-		       cout << "\n Ainda nao implementado! ";
 
-		       break;
-			}
-			case 3: {
-		       cout << "\n Ainda nao implementado! ";
-		       break;
-			}
-			case 4: {
-                cout << "Percorrendo em ordem...\n";
-                tree.inOrder(tree.getSource());
-                break;
-			}
-			case 5:{
-                break;
-			}
+		switch (option)
+		{
+		case 1:
+		{
+			cout << "\n Informe o valor (int) -> ";
+			cin >> x;
+			tree.insert(x);
+			break;
+		}
+		case 2:
+		{
+			cout << "\n Imprimindo em ordem...";
+			tree.inOrder(tree.getSource());
+			break;
+		}
+		case 3:
+		{
+			cout << "\n Imprimindo em pré-ordem...";
+			tree.preOrder(tree.getSource());
+			break;
+		}
+		case 4:
+		{
+			cout << "\n Imprimindo em pós-ordem...";
+			tree.posOrder(tree.getSource());
+			break;
+		}
+		case 5:
+		{
+			int key;
+			cout << "Insira a chave para pesquisar recursivamente \n";
+			cin >> key;
+			cout << "\n Pesquisando recursivamente...";
 
-			default:
-				cout << "\n Opcao invalida! \n\n\n";
-        } // fim switch
-    } while(option != 5);
+			tree.recursiveSearch(tree.getSource(), key);
 
+			break;
+		}
+		case 6:
+		{
+			int key;
+			cout << "Insira a chave para pesquisar iterativamente \n";
+			cin >> key;
+			cout << "\n Pesquisando iterativamente...";
+
+			tree.iterativeSearch(tree.getSource(), key);
+
+			break;
+		}
+		case 7:
+		{
+			cout << tree.nodeCounter(tree.getSource());
+			break;
+		}
+		case 8:
+		{
+			cout << tree.treeHeight();
+			break;
+		}
+		case 9:
+		{
+			cout << tree.minimumNode()->getKey();
+			break;
+		}
+		case 10:
+		{
+			cout << tree.maximumNode()->getKey();
+			break;
+		}
+		case 11:
+		{
+			tree.keyLeafs(tree.getSource());
+			break;
+		}
+		case 12:
+		{
+			int key;
+			cout << "Insira a chave para deletar \n";
+			cin >> key;
+			cout << "\n Deletando chave...";
+
+			tree.deleteLeaf(key);
+			break;
+		}
+
+		default:
+			cout << "\n Opcao invalida! \n\n\n";
+		} // fim switch
+	} while (option != 0);
 
 	return 0;
 }
